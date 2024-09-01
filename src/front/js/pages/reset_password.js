fd1b9d392fa275e9a141ac33b634eaa80791f911
Carmen Morcuende <166156217+carmenmorc@users.noreply.github.com>
2024-09-01 12:49:38 +0000

    CSS Reset Password

--- src/front/js/pages/reset_password.js
+++ src/front/js/pages/reset_password.js
@@ -1,56 +1,90 @@
 import React, { useContext, useState } from "react";
 import { useNavigate, useSearchParams } from "react-router-dom";
-
+import "../../styles/reset_password.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { Context } from "../store/appContext";
 
 export const Reset_password = () => {
     const { store, actions } = useContext(Context);
-	const [params, setParams]=useSearchParams()
-	const navigate=useNavigate()
+    const [params] = useSearchParams();
+    const navigate = useNavigate();
+    const [showPassword, setShowPassword] = useState(false);
+    const [showConfirmPassword, setShowConfirmPassword] = useState(false);
 
-    async function submitForm(e){
-		e.preventDefault()
-		let formData= new FormData(e.target)
-		let password=formData.get("password")
-		let passwordConfirm=formData.get("passwordConfirm")
-		
-		if(password== passwordConfirm){
-			let baseUrl=process.env.BACKEND_URL
-			let resp= await fetch(baseUrl+"/api/changepassword", {
-				method: "PATCH",
-				headers: {
-                    "Content-Type":"application/json", 
-                    "Authorization":"Bearer "+params.get("token")},
-				body: JSON.stringify({password}) 
-			})
+    async function submitForm(e) {
+        e.preventDefault();
+        let formData = new FormData(e.target);
+        let password = formData.get("password");
+        let passwordConfirm = formData.get("passwordConfirm");
 
-			if(resp.ok){
-				console.log("Clave cambiada")
-				navigate("/")
-			}
-		}else{
-			console.log("Claves invalidas")
-		}
-	}
+        if (password === passwordConfirm) {
+            let baseUrl = process.env.BACKEND_URL;
+            let resp = await fetch(baseUrl + "/api/changepassword", {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Authorization": "Bearer " + params.get("token")
+                },
+                body: JSON.stringify({ password })
+            });
 
+            if (resp.ok) {
+                console.log("Password changed");
+                navigate("/login");
+            }
+        } else {
+            console.log("Invalid Password");
+        }
+    }
+
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
+    const toggleConfirmPasswordVisibility = () => {
+        setShowConfirmPassword(!showConfirmPassword);
+    };
 
     return (
-        <div style={{ backgroundColor: '#B4E49D', minHeight: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-            <div className="container" style={{ width: '80vw', maxWidth: '500px', backgroundColor: 'white', padding: '20px', borderRadius: '10px', marginTop: '55px' }}>
+        <div className="reset-password">
+            <div className="reset-password-container">
                 <h2>Reset Password</h2>
                 
                 <form onSubmit={submitForm}>
-                    <div className="mb-3">
+                    <div className="mb-3 reset-password-form">
                         <label>New Password</label>
-                        <input name="password" type="password" style={{ width: '100%', borderRadius: '10px', border: '1px solid #B4E49D', padding: '10px' }}/>
+                        <div className="position-relative">
+                            <input
+                                name="password"
+                                type={showPassword ? "text" : "password"}
+                                className="form-control"
+                            />
+                            <i
+                                className={`fa-solid ${showPassword ? "fa-eye" : "fa-eye-slash"} position-absolute`}
+                                style={{ right: '10px', top: '50%', transform: 'translateY(-50%)', cursor: 'pointer', color:'#075E81' }}
+                                onClick={togglePasswordVisibility}
+                            ></i>
+                        </div>
                     </div>
-                    <div className="mb-3">
+                    <div className="mb-3 reset-password-form">
                         <label>Confirm Password</label>
-                        <input name="passwordConfirm" type="password" style={{ width: '100%', borderRadius: '10px', border: '1px solid #B4E49D', padding: '10px' }}/>
-                        <div id="emailHelp" className="form-text">Confirme su clave.</div>
+                        <div className="position-relative">
+                            <input
+                                name="passwordConfirm"
+                                type={showConfirmPassword ? "text" : "password"}
+                                className="form-control"
+                            />
+                            <i
+                                className={`fa-solid ${showConfirmPassword ? "fa-eye" : "fa-eye-slash"} position-absolute`}
+                                style={{ right: '10px', top: '50%', transform: 'translateY(-50%)', cursor: 'pointer', color:'#075E81' }}
+                                onClick={toggleConfirmPasswordVisibility}
+                            ></i>
+                        </div>
+                        <div id="emailHelp" className="reset-password-form-text">Confirm your password</div>
                     </div>
                     
-                    <button type="submit" style={{ backgroundColor: '#B4E49D', border: 'none', borderRadius: '10px', padding: '10px', color: 'white' }}>Reset Password</button>
+                    <button type="submit" className="reset-password-form-btn">Reset Password</button>
                 </form>
             </div>
         </div>
